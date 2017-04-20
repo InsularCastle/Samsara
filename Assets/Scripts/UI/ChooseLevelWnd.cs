0100c65c56cf3337868eaa2ecaf17b2a4a389f97
InsularCastle <18621960356@163.com>
2017-04-21 02:17:22 +0800

    update Options

--- Assets/Scripts/UI/ChooseLevelWnd.cs
+++ Assets/Scripts/UI/ChooseLevelWnd.cs
@@ -11,6 +11,16 @@ public class ChooseLevelWnd : BaseWindow
 {
     public void Init()
     {
-        
+        Transform _OptionBtnParent = _wndTran.FindChild("Scroll View/Viewport/Content");
+        Transform _OptionBtn = _wndTran.FindChild("Scroll View/Option");
+
+        for (int i = 0; i < 4; i++)
+        {
+            Transform optionTrans = GameObject.Instantiate<GameObject>(_OptionBtn.gameObject).transform;
+            optionTrans.parent = _OptionBtnParent;
+            optionTrans.localPosition = Vector3.zero;
+            optionTrans.localScale = Vector3.one;
+            optionTrans.gameObject.SetActive(true);
+        }
     }
 }
